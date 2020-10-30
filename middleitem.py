d5f7cb2be7abb1d7802844dedc964f9dfe287a76
Phu Dinh <53196661+PhuDDinh@users.noreply.github.com>
2020-10-29 22:08:24 -0600

    fixed comment to have # in front

--- middleitem.py
+++ middleitem.py
@@ -1,6 +1,6 @@
 #Create a function called middle_element that has one parameter named lst.
 
-If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
+#If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
 
 def middle_element(lst):
   if len(lst) % 2 == 0:
